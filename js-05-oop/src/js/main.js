f095bad3f19de6de8ecfa7c27aebeb837c039282
Fatima Orozco <fatiorozd@gmail.com>
2025-12-03 14:03:11 -0600

    feat objetos 3-12-2025

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -1,1 +1,18 @@
-document.getElementById("title").innerText = "OOP in JavaScript, Cohorte 62";
+// Inportación nombrada
+import { changePrincipalTitle } from "./modules/changePrincipalTitle.js"; 
+
+changePrincipalTitle("title");
+
+// Crear una función que se le pase el nombre de la imagen a mostrar (cat o dino)
+// esta imagen es la que se debe mostrar en el elemento con id "logo"
+
+import {changeLogo} from "./modules/changeImage.js";
+//changeLogo("assets/img/cat.jpg")
+
+import { aliceUser } from "./modules/literalObject.js";
+
+console.log(aliceUser);
+console.log(aliceUser.stringify())
+
+import{User} from ""
+const fatima =  new User ()
